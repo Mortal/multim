5109d21fce017b5ef91b7bb7c8decbd0f23ddcc4
Mathias Rav <rav@cs.au.dk>
2016-02-26 10:03:18 +0100

    Put main code into a main() function

--- multi.py
+++ multi.py
@@ -21,35 +21,41 @@ import stat
 import subprocess
 import sys
 
-args = list(sys.argv[1:])
-parallel = '-j1'
-target_dir = None
 
-for i in range(len(args)):
-    arg = args[i]
-    if re.match(r'-j[0-9]*', arg):
-        parallel = arg
-        args[i] = ''
-        continue
-    try:
-        st = os.stat(arg)
-    except FileNotFoundError:
-        continue
+def main():
+    args = list(sys.argv[1:])
+    parallel = '-j1'
+    target_dir = None
 
-    if stat.S_ISDIR(st.st_mode):
-        if target_dir is None:
-            target_dir = arg
-            args[i] = '$@'
-        else:
-            print("Too many directories in command line")
+    for i in range(len(args)):
+        arg = args[i]
+        if re.match(r'-j[0-9]*', arg):
+            parallel = arg
+            args[i] = ''
+            continue
+        try:
+            st = os.stat(arg)
+        except FileNotFoundError:
+            continue
 
-filenames = (f for f in os.listdir(target_dir) if re.match(r'[^ ]*\.[^ ]*', f))
-make_command = ['make', '-B', '-f', '-', parallel]
+        if stat.S_ISDIR(st.st_mode):
+            if target_dir is None:
+                target_dir = arg
+                args[i] = '$@'
+            else:
+                print("Too many directories in command line")
 
-makefile = ('TARGETS := %s\nall: $(TARGETS)\n$(TARGETS):%%:\n\t%s' %
-            (' '.join(filenames),
-             ' '.join(args)))
+    filenames = (f for f in os.listdir(target_dir) if re.match(r'[^ ]*\.[^ ]*', f))
+    make_command = ['make', '-B', '-f', '-', parallel]
 
-with subprocess.Popen(make_command, stdin=subprocess.PIPE, cwd=target_dir) as proc:
-    proc.communicate(makefile.encode('utf8'))
-    proc.wait()
+    makefile = ('TARGETS := %s\nall: $(TARGETS)\n$(TARGETS):%%:\n\t%s' %
+                (' '.join(filenames),
+                 ' '.join(args)))
+
+    with subprocess.Popen(make_command, stdin=subprocess.PIPE, cwd=target_dir) as proc:
+        proc.communicate(makefile.encode('utf8'))
+        proc.wait()
+
+
+if __name__ == "__main__":
+    main()
